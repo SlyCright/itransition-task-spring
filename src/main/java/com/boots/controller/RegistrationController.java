71843d9c10ff16d58b6e0d6d7b1aaf3679da6a17
Vadim Kuleshov <v.kuleshov@dev.local>
2022-02-18 22:39:36 +0300

    heroku fixing trst

--- src/main/java/com/boots/controller/RegistrationController.java
+++ src/main/java/com/boots/controller/RegistrationController.java
@@ -9,15 +9,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
 
 @Controller
+@RequestMapping()
 public class RegistrationController {
 
     @Autowired
     private UserService userService;
+
+    @GetMapping("/")
+    public String index() {
+        return "login";
+    }
 
     @GetMapping("/registration")
     public String registration(Model model) {
